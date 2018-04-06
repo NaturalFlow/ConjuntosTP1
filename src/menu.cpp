e8aa130852f2d5e18e75c0024ba427239c3a9bff
L.Brito <31296430+NaturalFlow@users.noreply.github.com>
2018-04-06 13:15:36 -0300

    Delete menu.cpp

--- src/menu.cpp
+++
@@ -1,6 +0,0 @@
-void menuControl() {
-    showOptions("1 - União               2 - Intersecção",
-                "3 - Diferença A e B     4 - Diferença B e A",
-                "5 - A subconjunto de B  6 - B subconjunto de A");
-    switch
-}
